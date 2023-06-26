151bf62ab87377ae07a4d10699e8e28b8e869499
eroblaze <eboselumepaul@gmail.com>
2023-06-26 06:48:26 +0100

    Added DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -6,10 +6,12 @@
 
 using namespace std;
 using ll = long long;
-using VI = vector<int>;
-using VVI = vector<vector<int>>;
 using PII = pair<int, int>;
-using PQI = priority_queue<int>;
+using VI = vector<int>;
+using VLL = vector<ll>;
+using VVI = vector<VI>;
+using VVLL = vector<VLL>;
+using VPII = vector<PII>;
 
 // Reads N input into V
 template <typename T>
@@ -18,19 +20,12 @@ void create_vector(T&, int);
 template <typename T>
 void display_contents(const T&);
 
-struct Compare_sort {
-	bool operator() (const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
-		return get<2>(a) > get<2>(b);
-	}
-};
-
-// Worst time complexity
-struct Union_Find {
+// Worst time complexity -> Competitive Programming Book
+struct DSU1 {
 	vector<int> m_sets;
 	vector<int> m_sizes;
-	int m_val{0};
 
-	Union_Find(int N) {
+	DSU1 (int N) {
 		m_sets.resize(N+1);
 		m_sizes.resize(N+1);
 		fill(m_sizes.begin(), m_sizes.end(), 1);
@@ -47,30 +42,30 @@ struct Union_Find {
 	bool same(int a, int b) const {
 		return find(a) == find(b);
 	}
-	// O(n)
-	void merge(int a, int b, int weight) {
+	// O(n) UNION BY RANK OR SIZE
+	void merge(int a, int b) {
 		int rep_a = find(a);
 		int rep_b = find(b);
+
+		if (a_reps == b_reps) return;
 
 		if (m_sizes[rep_a] < m_sizes[rep_b]) swap(rep_a, rep_b);
 		m_sets[rep_b] = rep_a;
 		m_sizes[rep_a] += m_sizes[rep_b];
-		m_sizes[rep_b] = 1;
-
-		m_val += weight;
+		m_sizes[rep_b] = 0;
 	}
 
 };
 
-// Has better time complexity - From 'Galen Colin'
-struct DSU {
+// Has better time complexity than ^ -> From 'Galen Colin'
+struct DSU2 {
     int N{0};
     VI m_sets;
     VI m_sizes;
     VVI adj;
 	int m_val{0};
 
-    DSU(int n):N(n+1) {
+    DSU2(int n):N(n+1) {
         m_sets.resize(N);
         iota(m_sets.begin(), m_sets.end(), 0);
         m_sizes = VI(N, 1);
@@ -85,7 +80,7 @@ struct DSU {
     bool same(int a, int b) {
         return find(a) == find(b);
     }
-    // (n log(n))
+    // (n log(n)) UNION BY RANK OR SIZE
     void merge(int a, int b, int weight) {
         if (same(a, b)) return;
         int a_reps = m_sets[a];
@@ -117,16 +112,20 @@ private:
 
 };
 
-// PATH COMPRESSION - GeekForGeek
+// PATH COMPRESSION Best Complexity -> GeekForGeek
+// The two techniques - PATH COMPRESSION with the union by RANK/SIZE, 
+// the time complexity will reach nearly constant time. 
+// It turns out, that the final amortized time complexity is O(α(n)), 
+// where α(n) is the inverse Ackermann function, which grows very steadily 
+// (it does not even exceed for n<10600  approximately).
 struct DSU3 {
 	vector<int> m_sets;
 	vector<int> m_sizes;
-	int m_val{0};
 
 	DSU3 (int n) {
-		m_sets.resize(n+1);
+		m_sets.resize(n);
 		iota(m_sets.begin(), m_sets.end(), 0);
-		m_sizes = vector<int>(n+1, 1);
+		m_sizes = vector<int>(n, 1);
 	}
 
 	int find(int a) {
@@ -150,7 +149,7 @@ struct DSU3 {
 		return find(a) == find(b);
 	}
 	// UNION BY RANK OR SIZE
-	void merge(int a, int b, int weight) {
+	void merge(int a, int b) {
 		int a_reps = find(a);
 		int b_reps = find(b);
 
@@ -160,42 +159,14 @@ struct DSU3 {
 		m_sets[b_reps] = a_reps;
 		m_sizes[a_reps] += m_sizes[b_reps];
 		m_sizes[b_reps] = 0;
-
-		m_val += weight;
 	}
 };
 
 
-void kruskal(priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Compare_sort> edge_list, int N) {
-
-	// Union_Find obj(N);
-	DSU3 obj(N);
-	
-	while (!edge_list.empty()) {
-		tuple<int, int, int> edge = edge_list.top(); edge_list.pop();
-		int a,b,c;
-		tie(a,b,c) = edge;
-
-		if (!obj.same(a, b)) obj.merge(a,b,c);
-	}
-
-	int minimum_spanning_tree = obj.m_val;
-	db1(minimum_spanning_tree);
-}
-
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0);
 
-    int N = 6;
-	vector<tuple<int, int, int>> edge_list{{1,2,3}, {1,5,5}, {2,5,6}, {2,3,5}, {3,4,9}, {3,6,3}, {4,6,7}, {5,6,2}};
-
-	priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Compare_sort> sorted_edges;
-
-	for (auto edge: edge_list) {
-		sorted_edges.push(edge);
-	}
-
-	kruskal(sorted_edges, N);
+	int N; cin>>N;
 
     return 0;
 }
